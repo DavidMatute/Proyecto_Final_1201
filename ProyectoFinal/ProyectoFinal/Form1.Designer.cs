b75ce7de80341b93df1aabe8027b5ce1369a0c2c
DavidMatute <86425726+DavidMatute@users.noreply.github.com>
2021-08-09 20:45:40 -0600

    segundo commit
    creación de formulario del inicio de sesión

--- ProyectoFinal/ProyectoFinal/Form1.Designer.cs
+++ ProyectoFinal/ProyectoFinal/Form1.Designer.cs
@@ -28,12 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.toolStripTabItem1 = new Syncfusion.Windows.Forms.Tools.ToolStripTabItem();
+            this.toolStripTabItem2 = new Syncfusion.Windows.Forms.Tools.ToolStripTabItem();
+            this.toolStripTabItem3 = new Syncfusion.Windows.Forms.Tools.ToolStripTabItem();
+            this.SuspendLayout();
+            // 
+            // toolStripTabItem1
+            // 
+            this.toolStripTabItem1.Name = "toolStripTabItem1";
+            // 
+            // 
+            // 
+            this.toolStripTabItem1.Panel.AllowDrop = true;
+            this.toolStripTabItem1.Panel.Name = "";
+            this.toolStripTabItem1.Panel.ScrollPosition = 0;
+            this.toolStripTabItem1.Panel.TabIndex = 2;
+            this.toolStripTabItem1.Position = -1;
+            this.toolStripTabItem1.Size = new System.Drawing.Size(104, 19);
+            this.toolStripTabItem1.Tag = "1";
+            this.toolStripTabItem1.Text = "toolStripTabItem1";
+            // 
+            // toolStripTabItem2
+            // 
+            this.toolStripTabItem2.Name = "toolStripTabItem2";
+            // 
+            // 
+            // 
+            this.toolStripTabItem2.Panel.AllowDrop = true;
+            this.toolStripTabItem2.Panel.Name = "";
+            this.toolStripTabItem2.Panel.ScrollPosition = 0;
+            this.toolStripTabItem2.Panel.TabIndex = 3;
+            this.toolStripTabItem2.Position = -1;
+            this.toolStripTabItem2.Size = new System.Drawing.Size(104, 19);
+            this.toolStripTabItem2.Tag = "2";
+            this.toolStripTabItem2.Text = "toolStripTabItem2";
+            // 
+            // toolStripTabItem3
+            // 
+            this.toolStripTabItem3.Name = "toolStripTabItem3";
+            // 
+            // 
+            // 
+            this.toolStripTabItem3.Panel.AllowDrop = true;
+            this.toolStripTabItem3.Panel.Name = "";
+            this.toolStripTabItem3.Panel.ScrollPosition = 0;
+            this.toolStripTabItem3.Panel.TabIndex = 4;
+            this.toolStripTabItem3.Position = -1;
+            this.toolStripTabItem3.Size = new System.Drawing.Size(104, 19);
+            this.toolStripTabItem3.Tag = "3";
+            this.toolStripTabItem3.Text = "toolStripTabItem3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(690, 294);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Syncfusion.Windows.Forms.Tools.ToolStripTabItem toolStripTabItem1;
+        private Syncfusion.Windows.Forms.Tools.ToolStripTabItem toolStripTabItem2;
+        private Syncfusion.Windows.Forms.Tools.ToolStripTabItem toolStripTabItem3;
     }
 }
 
